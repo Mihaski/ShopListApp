3def13f71976e5ce3839c01756eb9e9409da909c
Pushka-Gonka <mzabrodin94@gmail.com>
2022-06-13 12:51:13 +0700

    create RecycleView adapter

---
+++ app/src/main/java/com/example/shoplistapp/presentation/ShopListAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.shoplistapp.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoplistapp.R
+import com.example.shoplistapp.domain.ShopItem
+
+class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    val list = listOf<ShopItem>()
+
+    class ShopItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+//        val layoutId = if (parent.shopItem.enabled) {
+//            R.layout.item_shop_enabled
+//        } else {
+//            R.layout.item_shop_disabled
+//        }
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_shop_enabled, parent, false)
+        return ShopItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        val shopItem = list[position]
+        holder.tvName.text = shopItem.name
+        holder.tvCount.text = shopItem.count.toString()
+        holder.itemView.setOnLongClickListener {
+            true
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
